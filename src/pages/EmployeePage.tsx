db1fb7431d8bed966450424adc437d1e157487fc
Ian dosal <crkdosal@gmail.com>
2026-08-02 09:23:02 +0800

    [feat] - auth - protect route authentication; add pages; add page not found for unknown route

---
+++ src/pages/EmployeePage.tsx
@@ -0,0 +1,11 @@
+import Employees from "../features/employee/Employees";
+
+function EmployeePage() {
+  return (
+    <div>
+      <Employees />
+    </div>
+  );
+}
+
+export default EmployeePage;
