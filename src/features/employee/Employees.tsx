2d794adf7ef99d86a66aed1f15106d613e228e2b
Ian dosal <crkdosal@gmail.com>
2026-08-14 10:59:19 +0800

    [feat] - employee - add feature employee summary card and implementation

--- src/features/employee/Employees.tsx
+++ src/features/employee/Employees.tsx
@@ -4,6 +4,7 @@ import EmployeeDetails from "./EmployeeDetails";
 import EmployeeCreateDialog from "./create/EmployeeCreateDialog";
 import Drawer from "../../components/drawer/Drawer";
 import EmployeeHeader from "./EmployeeHeader";
+import EmployeeSummary from "./EmployeeSummary";
 
 export default function Employees() {
   const [empId, setEmpId] = useState("");
@@ -11,7 +12,7 @@ export default function Employees() {
   return (
     <div className="flex flex-col gap-6 p-6 lg:p-8">
       <EmployeeHeader onSetShowModal={setShowModal} showModal={showModal} />
-
+      <EmployeeSummary />
       <EmployeeTable setEmpId={setEmpId} />
       {empId && (
         <Drawer
