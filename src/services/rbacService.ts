bd01ea1a1a774134ad0e46932291d7b62da79009
Ian dosal <crkdosal@gmail.com>
2026-09-05 22:57:14 +0800

    [feat]  - rbac - add getrople by id and type annotation of rolepermission response

--- src/services/rbacService.ts
+++ src/services/rbacService.ts
@@ -1,4 +1,7 @@
-import type { RoleResponse } from "../types/rbac-type";
+import type {
+  PermissionMatrixResponse,
+  RoleResponse,
+} from "../types/rbac-type";
 import { apiClient } from "./apiClient";
 
 export async function getRoles(query: {
@@ -7,5 +10,18 @@ export async function getRoles(query: {
   const res: RoleResponse = await apiClient.get("access-control/roles", {
     signal: query.signal,
   });
+  return res;
+}
+
+export async function getRolesById(query: {
+  id: string | null;
+  signal?: AbortSignal;
+}): Promise<PermissionMatrixResponse> {
+  const res: PermissionMatrixResponse = await apiClient.get(
+    `access-control/roles/${query.id}`,
+    {
+      signal: query.signal,
+    },
+  );
   return res;
 }
