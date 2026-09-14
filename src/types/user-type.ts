c8a2980c36193e2b8e5e38247f2770f44e9d7324
Ian dosal <crkdosal@gmail.com>
2026-09-14 17:05:10 +0800

    [feat] - user - add new user service list of users with pagination functionality and its type annotation

--- src/types/user-type.ts
+++ src/types/user-type.ts
@@ -18,3 +18,58 @@ export type Employee = {
   lastName: string;
   empId: string; // "EMP-003"
 };
+export type UserStatus = "PENDING" | "ACTIVE" | "SUSPENDED" | "HOLD";
+
+export type ResponseStatus = "success";
+
+export interface UserRoleType {
+  id: string;
+  role: RoleType;
+}
+
+export interface RoleType {
+  id: string;
+  roleName: string;
+  roleCode: string;
+}
+
+export interface DepartmentHeadType {
+  firstName: string;
+  lastName: string;
+}
+
+export interface DepartmentType {
+  departmentName: string;
+  departmentHead: DepartmentHeadType;
+}
+
+export interface User {
+  id: string;
+  email: string;
+  status: UserStatus;
+  createdByUserId: string | null;
+  activatedAt: string | null;
+  passwordChangedAt: string | null;
+  userRoles: UserRoleType[];
+  createdAt: string;
+  updatedAt: string;
+  employee: Employee;
+  employeeId: string;
+  department: DepartmentType;
+}
+
+export interface PaginationType {
+  totalItems: number;
+  itemsPerPage: number;
+  totalPages: number;
+  currentPage: number;
+  previousPage: number | null;
+  nextPage: number | null;
+}
+
+export interface UserListReponse {
+  message: string;
+  status: ResponseStatus;
+  results: User[];
+  pagination: PaginationType;
+}
